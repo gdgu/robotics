075ad7062bb5dbcbf1030b51d2c55418cbd00128
Rahul Shyokand <33034184+rahulshyokand@users.noreply.github.com>
2019-11-16 11:31:01 +0530

    Add files via upload

---
+++ _90deg_lock.ino
@@ -0,0 +1,26 @@
+#include <Servo.h> 
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+void setup() 
+{ 
+  servo1.attach(4);
+  servo2.attach(7);
+  servo3.attach(9);
+  servo4.attach(11);
+} 
+
+void loop() 
+{ 
+    servo1.write(90);              
+    servo2.write(90);              
+    servo3.write(90);              
+    servo4.write(90);              
+    delay(1000);
+servo1.write(90);
+delay(1000);
+        
+} 
